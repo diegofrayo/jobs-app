a14c7a8990536609cf633f3cedd0bdb53b7a91a3
Alejandro Baez <nativo.alejandro@gmail.com>
2017-09-18 14:50:16 -0500

    initial commit

---
+++ actions/types.js
@@ -0,0 +1,5 @@
+export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
+export const FACEBOOK_LOGIN_FAILURE = 'facebook_login_failure';
+export const FETCH_JOBS = 'fetch_jobs';
+export const LIKE_JOB = 'like_job';
+export const CLEAR_LIKED_JOBS = 'clear_liked_jobs';
---
+++ reducers/likes_reducer.js
@@ -0,0 +1,23 @@
+import {
+	LIKE_JOB,
+	CLEAR_LIKED_JOBS
+} from '../actions/types';
+import { REHYDRATE } from 'redux-persist/constants';
+import _ from 'lodash';
+
+export default function(state = [], action) {
+	switch(action.type) {
+		case REHYDRATE:
+			return action.payload.likedJobs || [];
+		case LIKE_JOB:		
+			return _.uniqBy([
+				action.payload,
+				...state
+			], 'jobkey');
+		case CLEAR_LIKED_JOBS:
+			return [];
+		default:
+			return state;
+	}
+}
+
